f56248acdc4b4a220c5a64518e1832f70ca8bcbc
tikaradate <tikaradate@gmail.com>
2023-05-10 22:35:37 -0300

    Começa projeto

---
+++ pallet.cpp
@@ -0,0 +1,21 @@
+
+struct destino{
+    int id;
+    vector<int> demanda;
+}
+
+struct item{
+    int id;
+    int peso;
+}
+
+struct pallet{
+    int peso;
+}
+
+int main(){
+    vector<item> itens;
+    vector<vector<int>> produtores;
+    vector<destino> destinos;
+    vector<pallet> pallets;
+}
